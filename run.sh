d9c4d21bc7c49673db84a1a9594724f978e84ff8
Michael Rodov <rodov@hpe.com>
2016-12-01 23:23:52 +0200

    updated filtered users

--- run.sh
+++ run.sh
@@ -11,14 +11,14 @@ JSON_NAME=games.json
 LOG_NAME=ctf_games.log
 JSON_HOME=/home/rodov/git/urt-react/DATA
 PARSER_HOME=/home/rodov/git/urt-parser
-URT_HOME=/home/c/DATA/UrbanTerror42/q3ut4
+URT_HOME=/home/rodov/.q3a/q3ut4
 #/home/rodov/.q3a/q3ut4/home/c/DATA/UrbanTerror42/q3ut4
 cp $URT_HOME/$LOG_NAME $PARSER_HOME/logs/$LOG_NAME
 
 cd $PARSER_HOME/bin
 mvn clean install
 
-java -jar $PARSER_HOME/bin/target/urtlog-1.0.jar -l $PARSER_HOME/logs/$LOG_NAME -o $JSON_HOME/$JSON_NAME -lim 15 -types 7 -admin PanzerjagerTiger -exclude "Bonev|Panzerjager\sTiger|OHAD|kobii|dead\s?woman\s?walking|Shalom|Kilaka|\[\sA\sC\sE\s\]|RODOV.*|PanzerjagerTiger_.*"
+java -jar $PARSER_HOME/bin/target/urtlog-1.0.jar -l $PARSER_HOME/logs/$LOG_NAME -o $JSON_HOME/$JSON_NAME -lim 15 -types 7 -admin PanzerjagerTiger -exclude "Bonev|Panzerjager\sTiger|OHAD|kobii|dead\s?woman\s?walking|Shalom|Kilaka|\[\sA\sC\sE\s\]|RODOV.*|Tal|PanzerjagerTiger_.*|KING_.*|DOD"
 
 cd $JSON_HOME
 git add $JSON_NAME
